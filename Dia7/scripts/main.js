eae92732eba486794bb6e719727430371b37eec2
Danny200523 <danicrack0709@gmail.com>
2025-04-25 09:03:54 -0500

    feat: :rocket: adding changes in style

--- Dia7/scripts/main.js
+++ Dia7/scripts/main.js
@@ -26,7 +26,7 @@ function cards(){
     pepito.onreadystatechange = (function(){
         if (this.readyState === 4 && this.status === 200) {
             let response = JSON.parse(this.responseText);
-            card_hu.innerHTML = `<img class="img_human" style="width: 8vw;" " src="${response["cards"][0]["image"]}">`;
+            card_hu.innerHTML = `<img class="img_human" style="width: 12vw;" " src="${response["cards"][0]["image"]}">`;
             console.log("funciono")
         }
     });
