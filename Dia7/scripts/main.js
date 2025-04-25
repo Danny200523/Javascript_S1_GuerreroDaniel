c8d9d2907a03e0e46a00ee68957ce56a8e1cdc93
Danny200523 <danicrack0709@gmail.com>
2025-04-24 20:26:09 -0500

    feat: :rocket: adding changes day 7

---
+++ Dia7/scripts/main.js
@@ -0,0 +1,9 @@
+function mostrarRecuadro(){
+    const toggle = document.getElementById("help");
+    if (toggle.style.opacity==0){
+        toggle.style.opacity=1;
+    }
+    else{
+        toggle.style.opacity=0;
+    }
+}
