671475468bac9935b5f6b0063d938034bea56e57
Danny200523 <danicrack0709@gmail.com>
2025-04-25 08:14:01 -0500

    feat: :rocket: adding buttons

--- Dia7/scripts/main.js
+++ Dia7/scripts/main.js
@@ -10,31 +10,32 @@ function mostrarRecuadro(){
 
 function shufflecards(){
     const baraja = "5ods5xzzupvj";
-    let url = `https://deckofcardsapi.com/api/deck/${baraja}/draw/?count=2`;
-    let pepito = new XMLHttpRequest();
-    pepito.open(`PUT`, url);
-    pepito.onreadystatechange = function(){
-        if (this.readyState === 4 && this.status === 200){
-            let response = JSON.parse(this.responseText);
-        }
-    };
-};
-
-function cards_inicials(){
-    shufflecards();
-    let baraja = "5ods5xzzupvj";
-    let card_ma = document.getElementById("card_machine");
-    let card_hu = document.getElementById("card_human");
-    let url = `https://deckofcardsapi.com/api/deck/${baraja}/draw/?count=2`;
+    let url = `https://deckofcardsapi.com/api/deck/${baraja}/shuffle`;
     let pepito = new XMLHttpRequest();
     pepito.open(`GET`, url);
-    pepito.onreadystatechange = function(){
-        if (this.readyState === 4 && this.status === 200) {
-            let response = JSON.parse(this.responseText);
-            card_ma.innerHTML = `<img src="${response["cards"][0]["images"]["svg"]}">`;
-            card_hu.innerHTML = `<img src="${response["cards"][1]["images"]["svg"]}">`;
-            console.log("funciono")
-        }
-    };
     pepito.send();
 };
+
+function cards(){
+    shufflecards();
+    let baraja = "5ods5xzzupvj";
+    const card_hu = document.getElementById("card_human");
+    let url = `https://deckofcardsapi.com/api/deck/${baraja}/draw/?count=1`;
+    let pepito = new XMLHttpRequest();
+    pepito.open(`GET`, url);
+    pepito.onreadystatechange = (function(){
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(this.responseText);
+            card_hu.innerHTML = `<img class="img_human" style="width: 8vw;" " src="${response["cards"][0]["image"]}">`;
+            console.log("funciono")
+        }
+    });
+    pepito.send();
+};
+
+cards();
+
+const button = document.getElementById("cards_inicial")
+button.addEventListener('click', (e) => {
+    cards();
+})
