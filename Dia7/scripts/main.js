26d70a02c0fc9ae1e5bde7cb68764d6a069168f2
Danny200523 <danicrack0709@gmail.com>
2025-04-27 20:28:40 -0500

    feat: :rocket: funtions

--- Dia7/scripts/main.js
+++ Dia7/scripts/main.js
@@ -34,27 +34,85 @@ function cards(){
             console.log("funciono");
             if (response["value"]==="ACE"){
                 let valor_inicial = 1;
+                return valor_inicial;
             }
             else if (response["value"]==="JACK"){
                 let valor_inicial = 11;
+                return valor_inicial;
             }
             else if (response["value"]==="QUEEN"){
                 let valor_inicial = 12;
+                return valor_inicial;
             }
             else if (response["value"]==="KING"){
                 let valor_inicial = 13;
+                return valor_inicial;
             }
             else{
                 let valor_inicial = response["value"];
+                return valor_inicial;
             };
         }
     });
     pepito.send();
 };
 
+function new_card(){
+    let pepito = new XMLHttpRequest();
+    let baraja = "5ods5xzzupvj";
+    let nwi = document.getElementById("card_new");
+    let url = `https://deckofcardsapi.com/api/deck/${baraja}/draw/?count=1`;
+    pepito.open(`GET`, url);
+    pepito.onreadystatechange = (function(){
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(this.responseText);
+            nwi.innerHTML = `<img class="img_new" style="width: 12vw;" " src="${response["cards"][0]["image"]}">`;
+            if (response["value"]==="ACE"){
+                let valor_final = 1;
+                return valor_final;
+            }
+            else if (response["value"]==="JACK"){
+                let valor_final = 11;
+                return valor_final;
+            }
+            else if (response["value"]==="QUEEN"){
+                let valor_final = 12;
+                return valor_final;
+            }
+            else if (response["value"]==="KING"){
+                let valor_final = 13;
+                return valor_final;
+            }
+            else{
+                let valor_final = response["value"];
+                return valor_final;
+            };
+        }
+    });
+    pepito.send();
+}
+
 const button = document.getElementById("nueva_card_higher")
 button.addEventListener('click', (e) => {
-    console.log("holi")
+    new_card();
+    let vf = parseInt(new_card());
+    let vi = parseInt(cards());
+    if (vi < vf) {
+        console.log("gano")
+    }
+    else{
+        console.log("mocho")
+    }
 })
-
-cards();
+const button2 = document.getElementById("nueva_card_lower")
+button2.addEventListener('click', (a) => {
+    new_card();
+    let vf = new_card();
+    let vi = cards();
+    if (vf < vi) {
+        console.log("gano")
+    }
+    else{
+        console.log("mocho")
+    }
+})
