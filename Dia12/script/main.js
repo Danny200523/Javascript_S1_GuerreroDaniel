c4595465721731d0b90d26e2b32751760085c832
Danny200523 <danicrack0709@gmail.com>
2025-05-02 08:36:11 -0500

    feat: :sparkles: adding day 12

---
+++ Dia12/script/main.js
@@ -0,0 +1,19 @@
+import {getGeneralInfoPeople} from "./API/apiPeople.js";
+import {perfiles} from "./DOM/modPeople.js";
+
+let input_search = document.querySelector('#input_search');
+let main = document.querySelector('.main');
+
+addEventListener("DOMContentLoaded",async()=>{
+    let data = await getGeneralInfoPeople();
+    main.innerHTML = await perfiles(data);
+    console.log("holi");
+    addEventListener("input",async pepito =>{
+        const filtro = pepito.target.value.toLowerCase().trim();
+        const personasFiltradas= data.filter(persona =>{
+            return persona.name_full.toLowerCase().includes(filtro);
+        });
+        console.log(personasFiltradas);
+        main.innerHTML = await perfiles(personasFiltradas);
+    })
+})
