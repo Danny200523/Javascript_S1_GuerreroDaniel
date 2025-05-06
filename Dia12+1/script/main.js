b0ee3a69b0b4f66f84fd28354705bf5bb59c46fe
Danny200523 <danicrack0709@gmail.com>
2025-05-06 09:10:23 -0500

    feat: :hammer: day 14

--- Dia12+1/script/main.js
+++ Dia12+1/script/main.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const datosContenedor = document.querySelector('.opciones');
+    const taskInput = document.getElementById('taskInput');
+    const addTaskButton = document.getElementById('newTask');
+
     async function fetchData(){
         const res = await fetch('https://68027bc10a99cb7408e9bb67.mockapi.io/tareas')
         data = await res.json();
@@ -36,9 +39,61 @@ document.addEventListener('DOMContentLoaded',()=>{
             datosContenedor.appendChild(capDiv)
             }
         });
+        document.querySelectorAll('.terminado').forEach(button=>{
+            button.addEventListener('click',botoncompletado);
+        })
     }
 
+    async function botoncompletado(event){
+        const id = event.target.getAttribute('data-id')
+        await fetch(`https://68027bc10a99cb7408e9bb67.mockapi.io/tareas${id}`,{
+            method:'PUT',
+            Headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                status:'Ready'
+            })
+        });
+        const data = await fetchData();
+        displayCapsula(data);
+    }
+
+    async function botoneliminado(event){
+        const id = event.target.getAttribute('data-id');
+        console.log(id)
+        await fetch(`https://68027bc10a99cb7408e9bb67.mockapi.io/tareas${id}`,{
+        method:'DELETE'
+        });
+        const data = await fetchData();
+        displayCapsula(data);
+    }
+
+    async function addNewTask(){
+        const task = taskInput.value;
+        if(task.trim()===''){
+            return true;
+        }
+        else{
+            await fetch(`https://66df3340de4426916ee3dd7e.mockapi.io/tareas/`,{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    task,status:'On hold'
+                })
+
+            });
+            taskInput.value='';
+            const data = await fetchData();
+            displayCapsula(data);
+        }
+    }
+
+    addTaskButton.addEventListener('click',addNewTask);
     fetchData().then(data=>{
+        console.log(data);
         displayCapsula(data);
     })
 })
