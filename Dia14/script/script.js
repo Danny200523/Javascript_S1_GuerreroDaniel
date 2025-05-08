6f73d3c82cb5ad3be87190736aca3a00d0325ee9
Danny200523 <danicrack0709@gmail.com>
2025-05-07 20:04:34 -0500

    feat: :sparkles: adding changes

--- Dia14/script/script.js
+++ Dia14/script/script.js
@@ -4,40 +4,40 @@ function addCharacter() {
         <form>
             <div class="row mb-3">
                 <div class="col-6">
-                <label for="exampleInputEmail1" class="form-label">Nombre Del Personaje</label>
-                <input  type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="nameper" class="form-label">Nombre Del Personaje</label>
+                <input  type="text" class="form-control" id="nameper" aria-describedby="emailHelp">
               </div>
               <div class="col-6">
-                <label for="exampleInputEmail1" class="form-label">Nombre Del Actor</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="nameAct" class="form-label">Nombre Del Actor</label>
+                <input type="text" class="form-control" id="nameAct" aria-describedby="emailHelp">
               </div>
             </div>
             <div class="row mb-3">
                 <div class="col-6">
-                <label for="exampleInputEmail1" class="form-label">Edad Del Actor</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="edadAct" class="form-label">Edad Del Actor</label>
+                <input type="text" class="form-control" id="edadAct" aria-describedby="emailHelp">
               </div>
               <div class="col-6">
-                <label for="exampleInputEmail1" class="form-label">Ubicacion</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="ubicacion" class="form-label">Ubicacion</label>
+                <input type="text" class="form-control" id="ubicacion" aria-describedby="emailHelp">
               </div>
             </div>
             <div class="row mb-3">
                 <div class="col-6">
-                <label for="exampleInputEmail1" class="form-label">Poster</label>
-                <input  type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="pposter" class="form-label">Poster</label>
+                <input  type="text" class="form-control" id="pposter" aria-describedby="emailHelp">
               </div>
               <div class="col-6">
-                <label for="exampleInputEmail1" class="form-label">Ubicacion</label>
-                <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="datee" class="form-label">Ubicacion</label>
+                <input type="date" class="form-control" id="datee" aria-describedby="emailHelp">
               </div>
             </div>
             <div class="row mb-3">
                 <div class="col-12">
-                <label for="exampleInputEmail1" class="form-label">Productora</label>
-                <select  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <option value="1">MARVEL</option>
-                    <option value="2">DC</option>
+                <label for="product" class="form-label">Productora</label>
+                <select  class="form-control" id="product" aria-describedby="emailHelp">
+                    <option value="MARVEL">MARVEL</option>
+                    <option value="DCOMICS">DCOMICS</option>
                 </select>
               </div>
             </div>
@@ -67,9 +67,7 @@ function addCharacter() {
     const pp = document.getElementById('prp');
     pp.addEventListener('click',function suit(){
         const ns = document.getElementById('nss');
-        this.innerHTML=``;
-        ns.innerHTML = this.innerHTML+`
-        <div id="form" class="container my-5 p-5">
+        ns.innerHTML += `
         <form>
             <div class="row mb-3">
                 <div class="col-12">
@@ -80,10 +78,49 @@ function addCharacter() {
                     <div>
                 </div>
             </div>
-        </form>
-        </div>`;
+        </form>`;
+    const np = document.getElementById('nameper').value;
+    const na = document.getElementById('nameAct').value;
+    const ea = document.getElementById('edadAct').value;
+    const ub = document.getElementById('ubicacion').value;
+    const ppos = document.getElementById('pposter').value;
+    const dte = document.getElementById('datee').value;
+    const prod = document.getElementById('product').value;
+    const newHero = {
+        nameCharacter: np,
+        nameActor: na,
+        edadActor: ea,
+        ubicacion: ub,
+        poster: ppos,
+        date: dte
+    }})
+    return newHero;
+    }
+    data[prod].push(newHero);
+    async function fetchData() {
+        await fetch(`https://67f91aad094de2fe6ea07a32.mockapi.io/people/project/Super-Hero`);
+        data = await resizeBy.JSON();
+        return data;
+    }
+    async function putData(data) {
+        await fetch(`https://67f91aad094de2fe6ea07a32.mockapi.io/people/project/Super-Hero`,{
+            method:'PUT',
+            headers:{
+                'Content-Type':'application/json'
+                },
+            body:JSON.stringify({data})
+            });
+            console.log('apea')
+            if (!Response.ok){
+                console.log("Invalid api")
+            }
+        }
+    const gd = document.getElementById('buttonSave');
+    gd.addEventListener('click',e=>{
+        fetchData();
+        putData(data);
     })
-};
+
 const newHero = document.getElementById('buttonADD');
 newHero.addEventListener('click', function casa() {
     addCharacter();
